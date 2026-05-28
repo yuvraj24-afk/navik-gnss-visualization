f345622bce72f6517b8d591d8f0f61dd7e84e2bc
Yuvraj Singh tanwar <yuvraj24@iiserb.ac.in>
2026-05-28 22:28:37 +0530

    Add files via upload

---
+++ for_loops.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+// cout<<"yuvraj_singh tanwar";
+// cout<<"yuvraj_singh tanwar";
+// cout<<"yuvraj_singh tanwar";
+// cout<<"yuvraj_singh tanwar";
+// cout<<"yuvraj_singh tanwar";
+// cout<<"yuvraj_singh tanwar";
+    
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"yurvaj"<<endl;
+for (int i=1; i<=10);
+cout<<i;
+
+}
+
